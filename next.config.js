ce66bacc1b691c1d922ba990c339497e3129b735
nourgocoulibaly <cloonyx28@outlook.fr>
2025-03-08 14:18:09 +0000

    bug fix

--- next.config.js
+++ next.config.js
@@ -1,50 +1,7 @@
-const withMDX = require('@next/mdx')({
-	extension: /\.mdx?$/,
-	options: {
-		remarkPlugins: [],
-		rehypePlugins: [],
-	},
-})
-
-module.exports = withMDX({
-	pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+/** @type {import('next').NextConfig} */
+const nextConfig = {
 	reactStrictMode: true,
-	env: {
-		dir: '/',
-	},
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '/api/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.vercel.app',
-				pathname: '?app=portfolio-theme-jqe0jhmif-atlamors.vercel.app'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/badge/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.shields.io',
-				pathname: '/github/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.githubusercontent.com',
-				pathname: '/**'
-			},
-			{
-				protocol: 'https',
-				hostname: '**.medium.com',
-				pathname: '/**'
-			},
-		],
-		domains: ['placeholder-course.jpg'],
-	},
-})
+	swcMinify: true,
+}
+
+module.exports = nextConfig
